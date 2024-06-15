6b29119bf6201e84f2384f23da537268e1c125a9
seth111 <48691368+seth111@users.noreply.github.com>
2024-06-15 08:57:30 +0200

    reset repository to a previous version

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
-import json
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-
-def load_airports(filepath):
-    with open(filepath, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        return data['airports']
-    
-
-def plot_airports(airports):
-    fig, ax = plt.subplots(figsize=(15, 10))
-    m = Basemap(projection='merc', llcrnrlat=-60, urcrnrlat=85, llcrnrlon=-180, urcrnrlon=180, resolution='c')
-    m.drawcoastlines()
-    m.drawcountries()
-    m.fillcontinents(color='lightgray', lake_color='aqua')
-    m.drawmapboundary(fill_color='aqua')
-
-    for airport in airports:
-        lon, lat = airport['lon'], airport['lat']
-        x, y = m(lon, lat)
-        m.plot(x, y, 'bo', markersize=5)
-
-    plt.title('Airports on World Map')
-    plt.show()
-    
-def load_airports(filepath):
-    with open(filepath, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        print(data)  # Ajouter ceci pour voir la structure des données
-        return data['airports']
-
+import tkinter as tk
+from view.main_view import MainView
 
 def main():
-    airports_filepath = 'airports.json'
-    airports = load_airports(airports_filepath)
-    plot_airports(airports)
+    root = tk.Tk()
+    app = MainView(root)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
